b2bd78ff6ea5115fec4023701586ad4ec8902282
DINESH <93979305+Dinesh8447@users.noreply.github.com>
2022-08-21 10:56:43 +0530

    Add files via upload

---
+++ star bugs/script.js
@@ -0,0 +1,9 @@
+function imgslider(image){
+    document.querySelector('.starbucks').src=image;
+}
+
+
+function bgcolorchang(bgcolor){
+    const circle=document.querySelector('.bg-circle');
+    circle.style.background=bgcolor;
+}
